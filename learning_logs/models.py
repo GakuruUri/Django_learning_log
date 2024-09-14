04527c96c16b4fdc541c06372eda6ef9b187d6aa
Uri Gakuru <ugakuru@gerties.org>
2024-09-14 11:22:14 +0300

    updating learning log project

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Topic(models.Model):
     """ A topic the user is learning about."""
     text = models.CharField(max_length=200)
-    dete_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """ Return a string representation of the model."""
