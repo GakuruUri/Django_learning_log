8ea868ce881a29f8d262967ce4d891ca30638db0
GakuruUri <urigakuru@icloud.com>
2024-09-15 18:13:21 +0300

    Short entries

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,6 +1,17 @@
-from django.db import models
+"""
+18-2. 
+Short Entries: The __str__() method in the Entry model currently appends
+an ellipsis to every instance of Entry when Django shows it in the admin site
+or the shell. Add an if statement to the __str__() method that adds an ellipsis
+(continued)
+Getting Started with Django
+387only if the entry is longer than 50 characters. Use the admin site to add an
+entry that’s fewer than 50 characters in length, and check that it doesn’t have
+an ellipsis when viewed.
+"""
 
-# Create your models here.
+
+from django.db import models
 
 class Topic(models.Model):
     """ A topic the user is learning about."""
@@ -19,8 +30,38 @@ class Entry(models.Model):
 
     class Meta:
         verbose_name_plural = 'entries'
-
+    
     def __str__(self):
         """ Return a simple string representing the entry."""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
+
+
+# from django.db import models
+
+# # Create your models here.
+
+# class Topic(models.Model):
+#     """ A topic the user is learning about."""
+#     text = models.CharField(max_length=200)
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         """ Return a string representation of the model."""
+#         return self.text
+
+# class Entry(models.Model):
+#     """ Something specific learned about a topic."""
+#     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         verbose_name_plural = 'entries'
+
+#     def __str__(self):
+#         """ Return a simple string representing the entry."""
+#         return f"{self.text[:50]}..."
     
