0f11f5fac96a45d359254b56b33d5ae6ff59cb04
GakuruUri <urigakuru@icloud.com>
2024-09-17 20:34:02 +0300

    The new_topic Template

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -19,3 +20,21 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+
+def new_topc(request):
+    """ Add a new topic."""
+    if request.method != 'POST':
+        # No data submitter; create a blank form.
+        form = TopicForm()
+    else:
+        # Post data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+# Display a blank or valid form
+context = {'form': form}
+return render(request, 'learning_logs/new_topic.html', context)
