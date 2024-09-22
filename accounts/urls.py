93dc987607c5b920abd310e6887782cea19ff158
GakuruUri <urigakuru@icloud.com>
2024-09-22 14:38:16 +0300

    The Registration Page

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     # Include default auth urls.
     path('', include('django.contrib.auth.urls')),
+
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
